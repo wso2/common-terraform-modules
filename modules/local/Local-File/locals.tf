b179ac28a28f05938836f3a0cc5f4695640b9a92
Ranika <ranikamadurawe@gmail.com>
2023-10-19 11:44:13 +0530

    Add TLS and Random modules

---
+++ modules/local/Local-File/locals.tf
@@ -0,0 +1,14 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+locals {
+  file_path = substr(var.file_path, -1, 1) != "/" ? "${var.file_path}/" : var.file_path
+}
