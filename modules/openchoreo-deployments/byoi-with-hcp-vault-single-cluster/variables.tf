24e1debec792e83e3c76aefce5b19b3d25aa3abf
sachithkasthuriarachchi <sachithka@wso2.com>
2026-02-02 14:49:41 +0530

    Push registry credentials to vault

--- modules/openchoreo-deployments/byoi-with-hcp-vault-single-cluster/variables.tf
+++ modules/openchoreo-deployments/byoi-with-hcp-vault-single-cluster/variables.tf
@@ -25,3 +25,20 @@ variable "google_webhook_address" {
     error_message = "If set, google_webhook_address must not be an empty or whitespace-only string."
   }
 }
+
+variable "docker_registry_host" {
+  description = "The Docker registry host for pulling/pushing images."
+  type        = string
+}
+
+variable "docker_registry_username" {
+  description = "The Docker registry username for pulling images."
+  type        = string
+  sensitive   = true
+}
+
+variable "docker_registry_password" {
+  description = "The Docker registry password for pulling images."
+  type        = string
+  sensitive   = true
+}
