724143990a00bfe7c6c4071f4ab63ad007a28fe9
udhanMti <udhanisuranga@gmail.com>
2026-03-05 15:48:57 +0530

    Address linter issues by adding required version requirement

--- modules/helm/Helm-Release/versions.tf
+++ modules/helm/Helm-Release/versions.tf
@@ -18,6 +18,7 @@
 # --------------------------------------------------------------------------------------
 
 terraform {
+  required_version = ">= 1.10"
   required_providers {
     helm = {
       source  = "hashicorp/helm"
--- modules/pkcs12/PKCS12-File/versions.tf
+++ modules/pkcs12/PKCS12-File/versions.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 1.10"
   required_providers {
     pkcs12 = {
       source  = "chilicat/pkcs12"
