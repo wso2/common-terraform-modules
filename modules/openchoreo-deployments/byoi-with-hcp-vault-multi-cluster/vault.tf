bce2a9f002a8acb56abd63529d1fd10272b98ee5
udhanMti <udhanisuranga@gmail.com>
2026-04-02 12:32:53 +0530

    Fix TF formatting issues

--- modules/openchoreo-deployments/byoi-with-hcp-vault-multi-cluster/vault.tf
+++ modules/openchoreo-deployments/byoi-with-hcp-vault-multi-cluster/vault.tf
@@ -99,9 +99,9 @@ module "external-secrets-write-app-role" {
 }
 
 resource "vault_kv_secret_v2" "oc_system_secrets" {
-  count     = local.is_vault ? 1 : 0
-  mount     = module.secrets-mount[0].path
-  name      = "system"
+  count = local.is_vault ? 1 : 0
+  mount = module.secrets-mount[0].path
+  name  = "system"
   data_json = jsonencode(
     merge(
       local.base_secrets,
